4d7177cb2aae95677e2fe6455e8a7b9429316d9e
Weston <dogooddowell@protonmail.com>
2024-04-09 17:51:05 -0500

    build widget app

---
+++ examples/nextjs14/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+        return (
+            <footer style={{ backgroundColor: '#FF66C4', color: 'white', textAlign: 'center', padding: '10px' }}>
+                <p>© JUST JUMPER IT - Multi-Chain One Stop Degen Swap</p>
+            </footer>
+        );
+    };
+    
+    export default Footer;
--- examples/nextjs14/app/page.tsx
+++ examples/nextjs14/app/page.tsx
@@ -1,11 +1,22 @@
 import { Widget } from './components/Widget';
 import { WidgetEvents } from './components/WidgetEvents';
+import Footer from './components/Footer';
 
 export default function Home() {
   return (
-    <main>
+    <main style={{ backgroundColor: 'black', color: 'white' }}>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          padding: '10px',
+        }}
+      >
+        <h1 style={{ color: '#FF66C4' }}>Just Jumper It!</h1>
+      </div>
       <WidgetEvents />
       <Widget />
+      <Footer />
     </main>
   );
 }
